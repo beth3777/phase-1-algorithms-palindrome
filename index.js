3740b77364d6745dbf1b2f229d6a846439f213db
email <bethkimaku@gmail.com>
2023-04-12 08:02:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 function isPalindrome(word) {
+  let left=0;
+  let right=word.length-1;
+  while (left<right){
+    if(word[left]!==word[right])
+    return false ;
+    left++
+    right--
+  }
+  return true
   // Write your algorithm here
 }
-
+isPalindrome('abba')
 /* 
+function isPalindrome(word)
+//store the length of the string 
+wordLength=word.length
+//initialize the loop from 0to length/2
+for i=o to wordLength/2
+//check if the character at position i is the same as the character at position wordLength-i-1
+if word[1]!==word[wordLength-1-i]
+//if the charactersare not equal,then return false 
+// if the loop ends with any returning false ,return true
   Add your pseudocode here
 */
 
